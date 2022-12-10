3d87ae87c97e4695e89935421fccecfddec9fb9e
Niv Stibel <Nivsvb@gmail.com>
2022-12-10 16:35:27 +0200

    moved filter to a diff component

---
+++ front/src/pages/home/components/Filter.tsx
@@ -0,0 +1,61 @@
+import { Person } from "../../../services/personService";
+import { TextField } from "@mui/material";
+import { Autocomplete } from "@mui/material";
+
+type FilterProps = {
+    textValue: string,
+    setTextValue: React.Dispatch<React.SetStateAction<string>>,
+    persons: Person[],
+    filterdPersons: Person[],
+    setFilterdPersons: React.Dispatch<React.SetStateAction<Person[]>>,
+    setAddButtonActive: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export default function Filter(params: FilterProps) {
+    const filterByValue = (vaule: string) => {
+        if (!vaule) {
+            params.setTextValue("");
+            params.setAddButtonActive(false);
+            params.setFilterdPersons(params.persons ?? []);
+        }
+        else {
+            params.setTextValue(vaule);
+            const filterd = params.persons?.filter(person => person.name.toLowerCase().includes(vaule.toLowerCase()))
+            params.setFilterdPersons(
+                filterd ?? []
+            )
+            if (!params.filterdPersons.find(person => person.name.toLowerCase() === vaule.toLowerCase()))
+                params.setAddButtonActive(true);
+            else
+                params.setAddButtonActive(false);
+        }
+    }
+
+    const handleSearch = (event: React.FormEvent<HTMLDivElement>) => {
+        filterByValue((event.target as HTMLInputElement).value);
+    }
+
+    const handleBlue = (event: React.FormEvent<HTMLDivElement>) => {
+        filterByValue((event.target as HTMLInputElement).value);
+    }
+
+    const handleOnChange = (event: React.SyntheticEvent<Element, Event>, value: (string | null)) => {
+        if (value) {
+            console.log(value);
+            filterByValue(value);
+        }
+    }
+    return (
+        <Autocomplete
+            id="free-solo-demo"
+            freeSolo
+            value={params.textValue}
+            options={[]}
+            onInput={handleSearch}
+            onChange={handleOnChange}
+            onBlur={handleBlue}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Search" />}
+        />
+    )
+}
--- front/src/pages/home/home.tsx
+++ front/src/pages/home/home.tsx
@@ -7,91 +7,41 @@ import PersonList from "./components/personList";
 import { RootState } from "../../store";
 import { useDispatch, useSelector } from 'react-redux'
 import { addPerson, selectCount } from "../../reducers/personSlice";
+import Filter from "./components/Filter";
 
 export default function Home(props: any) {
   const [filterdPersons, setFilterdPersons] = React.useState<Array<Person>>([]);
-  const [addButton, setAddButton] = React.useState<boolean>(false);
+  const [addButtonActive, setAddButtonActive] = React.useState<boolean>(false);
   const [textValue, setTextValue] = React.useState<string>("");
   const [persons, setPersons] = React.useState<Array<Person>>([]);
   const statePersons = useSelector(selectCount);
   const dispatch = useDispatch();
 
-  
+
   useEffect(() => {
     setPersons(statePersons);
-    console.log(statePersons);
-    
   }, [statePersons]);
-  
+
   useEffect(() => {
     setFilterdPersons(persons);
   }, [persons]);
 
-  const filterByValue = (vaule: string) => {
-    if (!vaule) {
-      setTextValue("");
-      setAddButton(false);
-      setFilterdPersons(persons ?? []);
-    }
-    else {
-      setTextValue(vaule);
-      const filterd = persons?.filter(person => person.name.toLowerCase().includes(vaule.toLowerCase()))
-      setFilterdPersons(
-        filterd ?? []
-      )
-      if(!filterdPersons.find(person => person.name.toLowerCase() === vaule.toLowerCase()))
-        setAddButton(true);
-      else
-        setAddButton(false);
-    }
-    console.log(filterdPersons);
-
-  }
-
-  const handleSearch = (event: React.FormEvent<HTMLDivElement>) => {
-    filterByValue((event.target as HTMLInputElement).value);
-  }
-
-  const handleBlue = (event: React.FormEvent<HTMLDivElement>) => {
-    filterByValue((event.target as HTMLInputElement).value);
-  }
-
-  const handleOnChange = (event: React.SyntheticEvent<Element, Event>, value: (string | null)) => {
-    if (value) {
-      console.log(value);
-      filterByValue(value);
-    }
-  }
-
   const handlePersonAddClick = async () => {
     const person = await personService.addPerson(textValue);
-    if(!person)
+    if (!person)
       return;
     if (person) {
       dispatch(addPerson(person));
     }
     console.log(persons);
     setTextValue("");
-    filterByValue('');
   }
 
   return (
     <div>
       <div className="flex justify-center mtb-1 gap-1">
-        {/* <Button onClick={() => dispatch(addPerson())}>Add Person</Button> */}
-        <Autocomplete
-          id="free-solo-demo"
-          freeSolo
-          value={textValue}
-          // options={persons?.map((person) => person.name) ?? []}
-          options={[]}
-          onInput={handleSearch}
-          onChange={handleOnChange}
-          onBlur={handleBlue}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Search" />}
-        />
-        {addButton && <Button variant="contained" onClick={handlePersonAddClick}>Add Person</Button>}
+        <Filter textValue={textValue} setTextValue={setTextValue} persons={persons} filterdPersons={filterdPersons} setFilterdPersons={setFilterdPersons} setAddButtonActive={setAddButtonActive} />
+        {addButtonActive && <Button variant="contained" onClick={handlePersonAddClick}>Add Person</Button>}
       </div>
       <PersonList persons={filterdPersons} />
     </div >
